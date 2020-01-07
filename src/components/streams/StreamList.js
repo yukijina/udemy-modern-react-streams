3bcbfa4b74ed3ce7b5a353ce8eccc8d4883f2be5
yukijina <yukijina@gmail.com>
2020-01-06 18:29:41 -0800

    add props to currentUser and isSingedIn state

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions/index';
 
 
@@ -9,19 +10,43 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return <div className="right floated content">
+                <button className="ui button primary">Edit</button>
+                <button className="ui button negative">Delete</button>
+            </div>
+        }
+    }
+
     renderList() {
-        return this.props.streams.map(streams => {
+        return this.props.streams.map(stream => {
             return(
-                <div className="item" key={streams.id}>
+                <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {streams.title}
-                        <div className="description">{streams.description}</div>
+                        {stream.title}
+                        <div className="description">{stream.description}</div>
                     </div>
+
                 </div>
             )
         })
     }
+
+    renderCreate() {
+        if(this.props.isSignedIn) {
+            return (
+                <div style={{textAlign: "right"}}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    }
+
     render() {
         console.log(this.props.streams)
         return (
@@ -30,6 +55,7 @@ class StreamList extends React.Component {
                <div className="ui celled list">
                 {this.renderList()}
                </div>
+               {this.renderCreate()}
            </div>
        )
 
@@ -38,7 +64,9 @@ class StreamList extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-        streams: Object.values(state.streams)
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     }
 }
 
