2f13ac857ddcf042ef192595134552d948b4be34
yukijina <yukijina@gmail.com>
2020-01-06 19:50:44 -0800

    update editStream and correct typo fo fetchStream

--- src/actions/index.js
+++ src/actions/index.js
@@ -30,15 +30,16 @@ export const fetchStreams = () => async dispatch => {
 }
 
 export const fetchStream = (id) => async dispatch => {
-    const response = await streams.get(`/stream/${id}`)
+    const response = await streams.get(`/streams/${id}`)
 
     dispatch({ type: FETCH_STREAM, payload: response.data})
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: response.data })
+    history.push('/')
 }
 
 export const deleteStream = (id) => async dispatch => {
