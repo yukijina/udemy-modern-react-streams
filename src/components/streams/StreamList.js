9aa42582829abe40e156060fcd509392ebe8b76e
yukijina <yukijina@gmail.com>
2020-01-06 17:22:30 -0800

    add component and mapStateToProps

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions/index';
 
-const StreamList = () => {
-    return (
-        <div>StreamList</div>
-    )
+
+class StreamList extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderList() {
+        return this.props.streams.map(streams => {
+            return(
+                <div className="item" key={streams.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {streams.title}
+                        <div className="description">{streams.description}</div>
+                    </div>
+                </div>
+            )
+        })
+    }
+    render() {
+        console.log(this.props.streams)
+        return (
+           <div>
+               <h2>Streams</h2>
+               <div className="ui celled list">
+                {this.renderList()}
+               </div>
+           </div>
+       )
+
+    }
 }
 
-export default StreamList;
+const mapStateToProps = (state) => {
+    return {
+        streams: Object.values(state.streams)
+    }
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
