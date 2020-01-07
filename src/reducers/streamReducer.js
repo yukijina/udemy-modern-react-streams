40d2a7fddeeb8ce5709a893f418ba8534b7a8966
yukijina <yukijina@gmail.com>
2020-01-06 17:22:42 -0800

    correct typos

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -6,7 +6,8 @@ import _ from 'lodash';
 export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_STREAMS:
-            return {...setImmediate, ..._.mapKeys(action.payload, 'id')}
+            //mapKeys - array to object
+            return {...state, ..._.mapKeys(action.payload, 'id')}
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM:
