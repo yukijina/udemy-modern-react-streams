2f9d3095ce506d450e0711045118fd23cc960bf8
yukijina <yukijina@gmail.com>
2020-01-07 22:02:55 -0800

    create modal and add css. import history that makes modal hide

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import history from '../history';
 
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                testing testing testing
+        <div onClick={()=> history.push('/')} className="ui dimmer modals visible active">
+            {/* The above event won't be bubble up */}
+            <div onClick={(e)=> e.stopPropagation()} className="ui standard modal visible active">
+                <div className="header">
+                    Delete Stream
+                </div>
+                <div className="content">
+                    Are you sure you want to delete this stream?
+                </div>
+                <div className="actions">
+                    <button className="ui button primary">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
             </div>
         </div>,
         document.getElementById('modal')
