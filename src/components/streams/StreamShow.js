f3268974b48330cc4f9a09ca5f5f725ed442849d
yukijina <yukijina@gmail.com>
2020-01-13 18:15:08 -0800

    add video tag, update componentDidMount(featch streaming video), add componentDidUpdate & componentWillUnmount

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,22 +1,54 @@
 import React from 'react';
+// flv is downloading video streaming and convert to some file that can be played in html file
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
-    componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id)
+    constructor(props) {
+        super(props)
+
+        this.videoRef = React.createRef()
     }
+    componentDidMount() {
+        const {id} = this.props.match.params
+        this.props.fetchStream(id)
+        this.buildPlayer()
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer()
+    }
+
+    componentWillUnmount() {
+        this.player.destroy()
+    }
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        const {id} = this.props.match.params
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load();
+    }
+
 
     render(){
         if(!this.props.stream) {
             return <div>...Loading</div>
         }
-        
+
         const {title, description} = this.props.stream
 
         return (
             <div>
                 StreamShow
+                <video ref={this.videoRef} style={{ width: '100%' }} controls />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
